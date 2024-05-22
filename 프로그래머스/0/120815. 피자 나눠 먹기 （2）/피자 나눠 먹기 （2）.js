5a33225665369ec375257fd097bb2a10990915f3
Bomi Kim <96987560+ekki88@users.noreply.github.com>
2024-05-22 23:56:01 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    let cnt = 1;
+    while(true){
+        if(cnt * 6 % n === 0) {
+            return cnt;
+        }
+        cnt = cnt + 1 ;
+    }
+    return answer;
+}
