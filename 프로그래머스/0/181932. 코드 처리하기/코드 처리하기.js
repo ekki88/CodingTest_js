a7df5423a13bbaf093f325b7004b2fe63d4ff835
Bomi Kim <96987560+ekki88@users.noreply.github.com>
2026-06-23 22:54:00 +0900

    [level 0] Title: 코드 처리하기, Time: 2.35 ms, Memory: 51.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,25 @@
+function solution(code) {
+    let mode = 0;
+    let ret = "";
+    
+    for (let idx = 0; idx < code.length; idx++) {
+        if (mode === 0 ) {
+            if(code[idx] !== "1") {
+                if (idx % 2 === 0) {
+                    ret += code[idx]
+                } 
+            } else {
+                mode = 1;
+            }
+        } else {
+            if (code[idx] !== "1") {
+                if (idx % 2 === 1) {
+                    ret += code[idx]
+                }
+            } else {
+                mode = 0;
+            }
+        }
+    }
+    return ret === "" ? "EMPTY" : ret
+}
